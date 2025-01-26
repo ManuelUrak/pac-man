ec2f35ff9e732e34a868a839df670bda6a48b48f
Manuel Urak <manuel.urak88@gmail.com>
2025-01-26 17:44:52 +0100

    Added tilemap class and draw function and tested it

--- src/game.js
+++ src/game.js
@@ -1,12 +1,16 @@
 /* File for the core game functionalities */
 
+import TileMap from "./tilemap.js";
+
 const canvas = document.getElementById("gameCanvas");
 const ctx = canvas.getContext("2d");
+const tileSize = 32;
+const tileMap = new TileMap(tileSize);
 
 // Game loop
 
 function gameLoop() {
-  console.log("test");
+  tileMap.draw();
 }
 
 // Run the game at 75fps
---
+++ src/tilemap.js
@@ -0,0 +1,11 @@
+/* File for drawing the tilemap */
+
+export default class TileMap {
+  constructor(tileSize) {
+    this.tileSize = tileSize;
+  }
+
+  draw() {
+    console.log("draw");
+  }
+}
