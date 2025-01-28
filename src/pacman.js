17d26a0b0893b0229f2bb551b489664e2e929273
Manuel Urak <manuel.urak88@gmail.com>
2025-01-28 12:25:15 +0100

    Added keyboard inputs

--- src/pacman.js
+++ src/pacman.js
@@ -1,5 +1,7 @@
 /* File for defining Pac-Mans behaviour, drawing Pac-Man and animating Pac-Man*/
 
+import MovingDirection from "./moving-direction.js";
+
 export default class Pacman {
   constructor(x, y, tileSize, velocity, tileMap) {
     this.x = x;
@@ -7,6 +9,11 @@ export default class Pacman {
     this.tileSize = tileSize;
     this.velocity = velocity;
     this.tileMap = tileMap;
+
+    this.currentMovingDirection = null;
+    this.requestedMovingDirection = null;
+
+    document.addEventListener("keydown", this.#keydown);
 
     this.#loadPacmanImages();
   }
@@ -46,4 +53,48 @@ export default class Pacman {
     ];
     this.pacmanImageIndex = 0;
   }
+
+  //Getting the keyboard input
+
+  #keydown = (event) => {
+    //Key up input
+
+    if (event.keyCode == 38) {
+      if (this.currentMovingDirection == MovingDirection.down) {
+        this.currentMovingDirection = MovingDirection.up;
+      } else {
+        this.requestedMovingDirection = MovingDirection.up;
+      }
+    }
+
+    //Key down input
+
+    if (event.keyCode == 40) {
+      if (this.currentMovingDirection == MovingDirection.up) {
+        this.currentMovingDirection = MovingDirection.down;
+      } else {
+        this.requestedMovingDirection = MovingDirection.down;
+      }
+    }
+
+    //Key left input
+
+    if (event.keyCode == 37) {
+      if (this.currentMovingDirection == MovingDirection.right) {
+        this.currentMovingDirection = MovingDirection.left;
+      } else {
+        this.requestedMovingDirection = MovingDirection.left;
+      }
+    }
+
+    //Key right input
+
+    if (event.keyCode == 39) {
+      if (this.currentMovingDirection == MovingDirection.left) {
+        this.currentMovingDirection = MovingDirection.right;
+      } else {
+        this.requestedMovingDirection = MovingDirection.right;
+      }
+    }
+  };
 }
